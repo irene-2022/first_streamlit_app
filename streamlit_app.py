9d7d360317679863322d2901bb795645bf2f94a3
irene-2022 <109168258+irene-2022@users.noreply.github.com>
2022-07-13 11:11:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
 
 streamlit.title("SnowPro Ninja's first appearance :) ")
+streamlit.text("[Psalm 133:1] How good and pleasant it is when God's people live together in unity.")
+streamlit.text("[Proverbs 1:7] The fear of the Lord is the beginning of knowledge.")
