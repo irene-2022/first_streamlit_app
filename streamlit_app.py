484053d3a48b1f59d6aaa6bba5332fe8ca05b603
irene-2022 <109168258+irene-2022@users.noreply.github.com>
2022-07-13 15:38:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title("SnowPro Ninja's Kitchen")
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -7,3 +8,6 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+lst_fruit = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(lst_fruit)
