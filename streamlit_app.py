d81b5af3405a94ccf49a258031b6c6e703c9e28f
irene-2022 <109168258+irene-2022@users.noreply.github.com>
2022-07-13 11:35:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title("~*~*~*~*~ SnowPro Ninja's Kitchen Menu ~*~*~*~*~")
+streamlit.title(" SnowPro Ninja's Kitchen Menu")
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
