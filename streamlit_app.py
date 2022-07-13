caa5a57c04cd2cbe29fc9c84f9d40df0a6ecfff9
irene-2022 <109168258+irene-2022@users.noreply.github.com>
2022-07-13 11:14:49 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
 
 streamlit.title("SnowPro Ninja's first appearance :) ")
-streamlit.text("[Psalm 133:1] How good and pleasant it is when God's people live together in unity.")
-streamlit.text("[Proverbs 1:7] The fear of the Lord is the beginning of knowledge.")
+streatlit.header("[Psalm 133:1]")
+streamlit.text("How good and pleasant it is when God's people live together in unity.")
+streamlit.header("[Proverbs 1:7]")
+streamlit.text("The fear of the Lord is the beginning of knowledge.")
+streamlit.header("[Proverbs 23:15]")
+streamlit.text("My child, if your heart is wise, then my heart too will be happy.")
